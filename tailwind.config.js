9fe39495f71c6e29ac7b4d8287c8c687e07e0e14
Isaac Tsibu-Darko <isaactsibudarko@gmail.com>
2023-09-25 20:42:27 +0000

    application structure, tsconfig and tailwind config complete along with fundamental css rules

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,3 @@
-/** @type {import('tailwindcss').Config}
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-*/
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
@@ -15,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#00040f",
+        primary: "#161D31",
         secondary: "#00f6ff",
         dimWhite: "rgba(255, 255, 255, 0.7)",
         dimBlue: "rgba(9, 151, 124, 0.1)",
